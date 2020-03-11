30ae9ab4263f657f81600b335d0b921dd4275539
Damiano Chintala <damianochintala@gmail.com>
2020-03-11 18:03:22 +0200

    Updates

---
+++ precache-manifest.f1b45f8bfe9c973698f302222955cac6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5381470b90856a89f02da22f5d9016d5",
+    "url": "/dalc-learn/index.html"
+  },
+  {
+    "revision": "f0ed2c8c475fd690bd38",
+    "url": "/dalc-learn/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ecd412dd7b470af3114a",
+    "url": "/dalc-learn/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0ed2c8c475fd690bd38",
+    "url": "/dalc-learn/static/js/2.948b567b.chunk.js"
+  },
+  {
+    "revision": "cf5689887d395d116a38769473339df4",
+    "url": "/dalc-learn/static/js/2.948b567b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd412dd7b470af3114a",
+    "url": "/dalc-learn/static/js/main.7e564e3a.chunk.js"
+  },
+  {
+    "revision": "b4830a80931cbe0f3ade",
+    "url": "/dalc-learn/static/js/runtime-main.12af23ec.js"
+  },
+  {
+    "revision": "9b2553886462e3d0dc56c355234181e1",
+    "url": "/dalc-learn/static/media/caro1.9b255388.jpg"
+  },
+  {
+    "revision": "c1bcb20648049ff6f8a6ad73f59ae779",
+    "url": "/dalc-learn/static/media/caro2.c1bcb206.jpg"
+  },
+  {
+    "revision": "ac43d7d8a45d007925787f43f1420d9a",
+    "url": "/dalc-learn/static/media/caro3.ac43d7d8.jpg"
+  },
+  {
+    "revision": "a96adc2709d93876b8283a4a1546fcaf",
+    "url": "/dalc-learn/static/media/caro4.a96adc27.jpg"
+  },
+  {
+    "revision": "9a6919eb624682c4a19fef430ecdba67",
+    "url": "/dalc-learn/static/media/caro5.9a6919eb.jpg"
+  },
+  {
+    "revision": "3f6770a9f073463d5f109001d7f09534",
+    "url": "/dalc-learn/static/media/caro6.3f6770a9.jpg"
+  },
+  {
+    "revision": "9af88f2a7c234e63619cd06dafb23b1b",
+    "url": "/dalc-learn/static/media/civic.9af88f2a.jpg"
+  },
+  {
+    "revision": "a57ee3e27419d548e9e416808a47cd24",
+    "url": "/dalc-learn/static/media/enterprise.a57ee3e2.jpg"
+  },
+  {
+    "revision": "71a017be5305b20538b9603cc097aa96",
+    "url": "/dalc-learn/static/media/logo.71a017be.jpg"
+  },
+  {
+    "revision": "f7a8d32e7cc8cd6978f28b419b6fe431",
+    "url": "/dalc-learn/static/media/skills.f7a8d32e.jpg"
+  }
+]);
